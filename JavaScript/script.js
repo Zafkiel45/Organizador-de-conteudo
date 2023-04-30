c4ec07c7755ad12661115defcfa8c749bb16ee86
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2023-04-30 20:18:07 -0300

    Ajustes

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,6 +7,7 @@ const hidden_css = document.querySelector('#hidden_css');
 const button_about = document.querySelectorAll('.button_about');
 const hidden_about_html = document.querySelector('#hidden_html_about');
 const hidden_about_css = document.querySelector('#hidden_css_about')
+const about = document.querySelectorAll('.about')
 aside.style.height = `${body.clientHeight + 200}px`
 
 buttons.forEach(function(item, index){
@@ -17,6 +18,7 @@ buttons.forEach(function(item, index){
     });
 })
 
+// botão relacionado ao contaíner dos conteúdos
 button_about.forEach(function(item, index){
     item.addEventListener('click', function(){
         hiddenCardHtml(index)
@@ -24,31 +26,40 @@ button_about.forEach(function(item, index){
     })
 })
 
+// funções dos container 
 function hiddenCardHtml(h){
     if(h == 0) {
         if(hidden_about_html.classList.toggle('html_about')){
-            hidden_about_html.style.height = '520px'
+            hidden_about_html.style.height = '530px'
             hidden_about_html.style.padding = '1.25em'
+            about[0].style.borderBottomLeftRadius = '0px'
+            about[0].style.borderBottomRightRadius = '0px'
         } else {
             hidden_about_html.style.height = '0'
             hidden_about_html.style.padding = '0'
-
+            about[0].style.borderBottomLeftRadius = '1.25em'
+            about[0].style.borderBottomRightRadius = '1.25em'
+            about[0].style.transition = '2s'
         }   
     }
 }
 function hiddenCardCss(c){
     if(c == 1) {
         if(hidden_about_css.classList.toggle('css_about')){
-            hidden_about_css.style.height = '520px'
+            hidden_about_css.style.height = '550px'
             hidden_about_css.style.padding = '1.25em'
+            about[1].style.borderBottomLeftRadius = '0px'
+            about[1].style.borderBottomRightRadius = '0px'
         } else {
             hidden_about_css.style.height = '0'
             hidden_about_css.style.padding = '0'
-
+            about[1].style.borderBottomLeftRadius = '1.25em'
+            about[1].style.borderBottomRightRadius = '1.25em'
+            about[1].style.transition = '2s'
         }   
     }
 }
-
+// Sidebar
 function removeHTML(){
     if(hidden_html.style.height != 0){
         hidden_html.style.height = '0';
